a85081a2fed9ff80bdd15a51990a9df295e79c5e
Rubai Mushtaq <rubai.mushtaq@gmail.com>
2020-03-29 19:06:20 +0530

    misc

--- src/Addition.java
+++ src/Addition.java
@@ -4,7 +4,7 @@ public class Addition {
 
     public static void main(String[] args) {
         System.out.println("Enter 2 nos.");
-        Scanner scanner = new Scanner(System.in);
+        var scanner = new Scanner(System.in);
         var x = scanner.nextInt();
         var y = scanner.nextInt();
         var z = x + y;
--- src/ConvertToAsc.java
+++ src/ConvertToAsc.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ConvertToAsc {
     public static void main(String[] args) {
         System.out.println("Enter a character.");
-        Scanner sc = new Scanner(System.in);
+        var sc = new Scanner(System.in);
         var a = sc.next().charAt(0);
         System.out.println("Ascii value" + a);
     }
